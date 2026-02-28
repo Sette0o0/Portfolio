6fd0d03012cd9c1b7bfc0839c9bfe8c22b79ad5c
Rafael Sette de Araujo <138353834+Sette0o0@users.noreply.github.com>
2026-02-28 14:03:40 -0300

    feat: componente de layout das páginas

---
+++ my-app/src/components/Layout/Footer.tsx
@@ -0,0 +1,16 @@
+
+function Footer() {
+	return (
+		<>
+			<footer className={`container-fluid`}>
+				<div className={`d-flex flex-column align-items-center`}>
+					<p>
+						Feito por: Rafael Sette de Araujo
+					</p>
+				</div>
+			</footer>
+		</>
+	)
+}
+
+export default Footer
---
+++ my-app/src/components/Layout/Header.tsx
@@ -0,0 +1,18 @@
+
+function Header() {
+	return (
+		<>
+			<header className="container-fluid">
+				<nav>
+					<ul className={`d-flex flex-row list-unstyled column-gap-3`}>
+						<li>Início</li>
+						<li>Projetos</li>
+						<li>Sobre Mim</li>
+					</ul>
+				</nav>
+			</header>
+		</>
+	)
+}
+
+export default Header

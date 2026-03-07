9f62da9b2ae56be8141692d875fd04224bec01a3
Rafael Sette de Araujo <138353834+Sette0o0@users.noreply.github.com>
2026-03-06 21:40:17 -0300

    feat: botão para troca de light/dark mode

--- my-app/src/components/Layout/Header.tsx
+++ my-app/src/components/Layout/Header.tsx
@@ -1,15 +1,17 @@
+import ThemeToggle from "../ThemeToggle"
 
 function Header() {
 	return (
 		<>
-			<header className="d-flex p-3">
-				<nav>
+			<header className="d-flex align-items-center p-3">
+				<nav className={`me-auto`}>
 					<ul className={`d-flex flex-row list-unstyled column-gap-3 m-0`}>
 						<li>Início</li>
 						<li>Projetos</li>
 						<li>Sobre Mim</li>
 					</ul>
 				</nav>
+				<ThemeToggle />
 			</header>
 		</>
 	)
---
+++ my-app/src/components/ThemeToggle/index.tsx
@@ -0,0 +1,40 @@
+import { useId } from "react"
+import { useTheme } from "../../context/Theme"
+import styles from "./index.module.css"
+
+function ThemeToggle() {
+  const { theme, toggleTheme } = useTheme()
+  const maskId = useId()
+
+  return(
+    <>
+      <div onClick={toggleTheme} className={`${styles.themeToggle} ${theme == "dark" && styles.dark}`}>
+        <svg width="30" height="30" viewBox="0 0 24 24">
+
+          {/* máscara que faz parte do círculo principal sumir */}
+          <mask id={maskId}>
+            <rect x="0" y="0" width="24" height="24" fill="white"/>
+            <circle className={`${styles.moonCut}`} cx="16.5" cy="9.5" r="5" />
+          </mask>
+
+          {/* círculo principal do sol e da lua */}
+          <circle cx="12" cy="12" r="6" mask={`url(#${maskId})`} fill="currentColor" />
+
+          {/* as linhas ao redor do sol */}
+          <g className={`${styles.rays}`} stroke="currentColor" stroke-width="2" stroke-linecap="round">
+            <line x1="12" y1="4" x2="12" y2="1" />
+            <line x1="17.66" y1="6.34" x2="19.78" y2="4.22" />
+            <line x1="20" y1="12" x2="23" y2="12" />
+            <line x1="17.66" y1="17.66" x2="19.78" y2="19.78" />
+            <line x1="12" y1="20" x2="12" y2="23" />
+            <line x1="6.34" y1="17.66" x2="4.22" y2="19.78" />
+            <line x1="4" y1="12" x2="1" y2="12" />
+            <line x1="6.34" y1="6.34" x2="4.22" y2="4.22" />
+          </g>
+        </svg>
+      </div>
+    </>
+  )
+}
+
+export default ThemeToggle
